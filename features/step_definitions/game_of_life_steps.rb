2997769926962cb7e229d038b7b1a4e99aaa0de2
cskksc <ckoparkar@live.in>
2014-12-05 15:46:18 +0530

    Dead cell with dead neighbors stays dead.

--- features/step_definitions/game_of_life_steps.rb
+++ features/step_definitions/game_of_life_steps.rb
@@ -8,3 +8,16 @@ end
 Then(/^I should see "(.*?)"$/) do |arg1|
   expect(@hello).to eq "hello"
 end
+
+Given(/^the following setup$/) do |table|
+  @data = table.raw
+  @game = GameOfLife.new(data: @data)
+end
+
+When(/^I evolve the board$/) do
+  @game.evolve_game
+end
+
+Then(/^the center cell should be dead$/) do
+  @game.is_dead?(@data.size/2, @data[0].size/2)
+end
--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,5 +1,73 @@
-module GameOfLife
+require 'pry'
+
+class GameOfLife
   def self.hello
     "hello"
   end
+
+  def initialize(args)
+    @data = args[:data]
+  end
+
+  def evolve_game
+    data = []
+    for x in (0...@data.size).to_a do
+      t = []
+      for y in (0...@data[0].size).to_a do
+        case
+        when is_dead?(x, y)
+          t << evolve_dead(x, y)
+        else
+          t << @data[x][y]
+        end
+      end
+      data << t
+    end
+    @data = data
+  end
+
+  def evolve_dead(x, y)
+    live_neighbors(neighbors(x, y)) == 3 ? 'x' : '.'
+  end
+
+  def is_dead?(x, y)
+    @data[x][y] == '.'
+  end
+
+  private
+
+  def get_range(n, limit)
+    n1 = [n - 1, 0].max
+    n2 = [n + 1, limit - 1].min
+    [n1, n2]
+  end
+
+  def dead_neighbors(ns)
+  end
+
+  def live_neighbors(ns)
+    ns.count('x')
+  end
+
+  def neighbors(x, y)
+    n = []
+    x1, x2 = get_range(x, @data.size)
+    y1, y2 = get_range(y, @data[0].size)
+    for r in (x1..x2).to_a do
+      for c in (y1..y2).to_a do
+        n << @data[r][c]
+      end
+    end
+    n.delete_at n.index(@data[x][y])
+    n
+  end
+
 end
+
+# data = [['.', '.', '.'],
+#         ['.', '.', '.'],
+#         ['.', '.', '.']]
+
+# game = GameOfLife.new(data: data)
+# p game.evolve_game
+# binding.pry
