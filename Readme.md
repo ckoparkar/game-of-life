da42a8be7e0ddc397186d4a95f6629819e68a7dc
cskksc <ckoparkar@live.in>
2014-12-06 18:10:38 +0530

    Visualize using Shoes. (No gem).

--- Readme.md
+++ Readme.md
@@ -3,3 +3,7 @@ An implementation of Game of Life in Ruby.
 Wiki: http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
 
 Project started to learn BDD.
+
+Cucumber features taken from: https://github.com/coreyhaines/practice_game_of_life.git
+
+Shoes visualizer taken from: https://github.com/elisehuard/game_of_life.git
---
+++ lib/life_shoes.rb
@@ -0,0 +1,52 @@
+#
+# This is a Shoes app to visualize your game of life.
+#
+
+## REQUIRE YOUR OWN CLASS HERE ##
+require_relative 'lib/game_of_life.rb'
+require 'shoes'
+W = 20 # cell width
+
+Shoes.app :title => 'Game of Life'do
+  background khaki
+  stroke black
+
+  def initialize_cells(life)
+    @height = life.size
+    @width = life.first.size
+    @cells = Array.new(@height){Array.new @width}
+    @height.times{|j| @width.times{|i| @cells[j][i] = rect 100+W*i, 50+W*j, W, W, :fill => white}}
+    @initialized = true
+  end
+
+  def show_cells
+    ## INSERT YOUR OWN CLASS WITH ITS ARGUMENTS HERE ##
+    grid = [['.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','x','x','x','.','.','.','x','x','x','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'],
+            ['.','x','.','.','.','.','x','.','x','.','.','.','.','x','.'],
+            ['.','x','.','.','.','.','x','.','x','.','.','.','.','x','.'],
+            ['.','x','.','.','.','.','x','.','x','.','.','.','.','x','.'],
+            ['.','.','.','x','x','x','.','.','.','x','x','x','.','.','.'],
+            ['.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','x','x','x','.','.','.','x','x','x','.','.','.'],            
+            ['.','x','.','.','.','.','x','.','x','.','.','.','.','x','.'],
+            ['.','x','.','.','.','.','x','.','x','.','.','.','.','x','.'],
+            ['.','x','.','.','.','.','x','.','x','.','.','.','.','x','.'],
+            ['.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'],
+            ['.','.','.','x','x','x','.','.','.','x','x','x','.','.','.']]
+
+    game = GameOfLife.new(grid: grid)
+    @e = every 1 do |n|
+      @n.text = "Generation: #{n}"
+      life = game.evolve_game
+      @initialized ||= initialize_cells(life)
+      @height.times{|j| @width.times{|i| @cells[j][i].style :fill => (life[j][i] == '.' ? white : green)}}
+    end
+  end
+
+  gb = stack
+  para link('start'){gb.clear{show_cells}}
+  para link('stop'){@e.stop}
+  @n = para
+end
